549cf62ba166f8ad4a1a7a711d567bf22796bb3a
DohaoSTR <muzalevskij.evgenij@mail.ru>
2025-01-08 22:39:50 +0300

    feat: обновление env файла для получения конфига из вышестоящей директории.

--- src/config/env.py
+++ src/config/env.py
@@ -1,3 +1,9 @@
 import environ
+import os
+
+from src.config.settings.base import BASE_DIR
+
+ENV_DIR = os.path.join(BASE_DIR.parent.parent, '.env')
 
 env = environ.Env()
+environ.Env.read_env(ENV_DIR)
