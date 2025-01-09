7d2e38237f2a2738bc2db351a3f503e5ea64fd52
Eugene Sergeevich Muzalevsky <53302920+DohaoSTR@users.noreply.github.com>
2025-01-09 16:56:00 +0300

    feat: добавил настройки для ограничения доступа к api.

--- src/config/settings/auth.py
+++ src/config/settings/auth.py
@@ -17,6 +17,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+THROTTLE_RATES_ANON = env.str('API_THROTTLE_RATES_ANON', default='10/minute')
+THROTTLE_RATES_USER = env.str('API_THROTTLE_RATES_USER', default='5000/hour')
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -26,14 +29,17 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '10/minute',
-        'user': '5000/hour',
+        'anon': THROTTLE_RATES_ANON,
+        'user': THROTTLE_RATES_USER,
     }
 }
 
+ACCESS_TOKEN_LIFETIME = env.int('API_ACCESS_TOKEN_LIFETIME', default=30)
+REFRESH_TOKEN_LIFETIME = env.int('API_REFRESH_TOKEN_LIFETIME', default=1440)
+
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=ACCESS_TOKEN_LIFETIME),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=REFRESH_TOKEN_LIFETIME),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
