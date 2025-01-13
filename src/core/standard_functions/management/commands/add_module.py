68bab1b0c0a55b9adb75fb5b0dbaf3d1a2f05c38
Eugene Sergeevich Muzalevsky <53302920+DohaoSTR@users.noreply.github.com>
2025-01-13 17:01:50 +0300

    feat: создание оптимальной структуры файлов при запуске команды add_module.

--- src/core/standard_functions/management/commands/add_module.py
+++ src/core/standard_functions/management/commands/add_module.py
@@ -1,47 +1,84 @@
+"""
+Команда для создания нового модуля в директории src/external_modules.
+
+Этот файл содержит Django команду для создания нового модуля в указанной директории.
+Команда создает необходимую структуру директорий и файлов для нового модуля.
+
+Пример использования:
+>>> python manage.py add_module my_new_app
+"""
+
 import os
 from textwrap import dedent
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from src.config.auto_config import check_app_config_name
+from src.core.standard_functions.methods import convert_snake_to_camel
 
 class Command(BaseCommand):
-    help = 'Создание нового Django приложения в директории src/external_modules без файла admin.py'
+    """
+    Класс Django команды для создания нового модуля.
+
+    Атрибуты:
+        help (str): Описание команды.
+    """
+    help = 'Создание нового модуля в директории src/external_modules'
 
     def add_arguments(self, parser):
-        parser.add_argument('name', help='Имя приложения')
+        """
+        Добавляет аргументы для команды.
+
+        Аргументы:
+            parser (argparse.ArgumentParser): Объект парсера аргументов.
+        """
+        parser.add_argument('name', help='Имя модуля')
 
     def handle(self, *args, **options):
-        app_name = options['name']
-        directory = 'src/external_modules'
+        """
+        Основной метод для выполнения команды.
 
-        # Убедитесь, что целевая директория существует
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        Аргументы:
+            *args: Дополнительные аргументы.
+            **options: Опции команды.
+        """
+        module_name = options['name']
 
-        app_directory = os.path.join(directory, app_name)
-        app_directory = os.path.normpath(app_directory)
-        
+        external_modules_directory = getattr(settings, 'EXTERNAL_MODULES_DIR', None)
+
+        # Проверка на наличие целевой директории
+        if not os.path.exists(external_modules_directory):
+            os.makedirs(external_modules_directory)
+
+        module_directory = os.path.join(external_modules_directory, module_name)
+        module_directory = os.path.normpath(module_directory)
+
         # Проверка существования модуля
-        if os.path.exists(app_directory):
-            self.stdout.write(self.style.ERROR(f'Приложение {app_name} уже существует по пути - {app_directory}'))
+        if os.path.exists(module_directory):
+            self.stdout.write(self.style.ERROR(f'Модуль {module_name} уже существует по пути - {module_directory}.'))
+            return
+        
+        camel_module_name = convert_snake_to_camel(module_name)
+        if check_app_config_name(external_modules_directory, camel_module_name):
+            self.stdout.write(self.style.ERROR(f'Уже существует модуль c именем класса конфига - {camel_module_name}Config.'))
+            self.stdout.write(self.style.ERROR(f'Попробуйте другое название модуля.'))
             return
 
-        # Создайте структуру директорий для нового приложения
-        os.makedirs(app_directory)
-        os.makedirs(os.path.join(app_directory, 'migrations'))
+        # Создание структуры директорий для нового приложения
+        os.makedirs(module_directory)
+        os.makedirs(os.path.join(module_directory, 'migrations'))
 
-        # Создайте файлы для нового приложения
+        # Создание файлов для нового приложения
         files_to_create = {
             '__init__.py': '',
             'apps.py': dedent(f"""
                 from django.apps import AppConfig
 
-                class {app_name.capitalize()}Config(AppConfig):
+                class {camel_module_name}Config(AppConfig):
                     default_auto_field = 'django.db.models.BigAutoField'
-                    name = 'src.external_modules.{app_name}'
+                    name = 'src.external_modules.{module_name}'
             """),
-            'models.py': '',
-            'tests.py': '',
-            'views.py': '',
             'urls.py': dedent("""
                 from django.urls import (
                     path
@@ -50,13 +87,43 @@ class Command(BaseCommand):
                 urlpatterns = [
                 ]
             """),
+            '.conf': dedent(f"""
+                [Main]
+                module_name = {camel_module_name}
+            """),
+            'models.py': dedent("""
+                from django.db import models
+
+                # Создавайте свои модели здесь.
+            """),
+            'scripts.py': dedent("""
+                # Создавайте свои скрипты здесь.
+            """),
+            'tests.py': dedent("""
+                from django.test import TestCase
+
+                # Создавайте свои тесты здесь.
+            """),
+            'serializers.py': dedent("""
+                from rest_framework import serializers
+
+                # Создавайте свои сериализаторы здесь.
+            """),
+            'methods.py': dedent("""
+                # Создавайте свои вспомогательные методы здесь.
+            """),
+            'views.py': dedent("""
+                from django.shortcuts import render
+
+                # Создавайте свои API методы здесь.
+            """),
             'migrations/__init__.py': '',
         }
 
         for filename, content in files_to_create.items():
-            file_path = os.path.join(app_directory, filename)
-            with open(file_path, 'w') as f:
-                f.write(content.strip())
+            file_path = os.path.join(module_directory, filename)
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(content.strip())
 
-        # Выведите сообщение об успешном создании приложения
-        self.stdout.write(self.style.SUCCESS(f'Приложение {app_name} успешно создано по пути - {directory}'))
+        # Вывод сообщения об успешном создании модуля
+        self.stdout.write(self.style.SUCCESS(f'Модуль {module_name} успешно создано по пути - {module_directory}'))
