22be55c45ba40d96dd9ce411d4ebba900f72b189
daMpoff <11projectone@gmail.com>
2025-04-11 00:24:54 +0300

    fix: Удаление базовой реализации шорткодов, из основного модуля

--- src/core/cms/models.py
+++ src/core/cms/models.py
@@ -16,16 +16,3 @@ class Object(models.Model):
     objectlink = models.CharField(max_length=255, default='')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.ForeignKey(Object_Type, on_delete=models.CASCADE)
-
-class ShortCode(models.Model):
-    name = models.CharField(max_length=100, default='')
-    content = models.TextField(default='')
-    ispage = models.BooleanField(default=False)
-    date_of_creation = models.DateField(default=timezone.now)
-    last_update = models.DateTimeField(default=timezone.now)
-    user =models.ForeignKey(User, on_delete=models.CASCADE)
-
-class ShortCode_Parameter(models.Model):
-    key = models.CharField(max_length=100, default='')
-    value = models.TextField(default='')
-    shortcode = models.ForeignKey(ShortCode, on_delete=models.CASCADE)
