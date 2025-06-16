66b0bde5af7898a5666675a4ff1b1e65bb47ad41
DohaoSTR <muzalevskij.evgenij@mail.ru>
2025-06-17 00:03:56 +0300

    build: удаление невостребованного кода.

--- src/external/examples/bpm/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class ExamplesBpmConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.external.examples.bpm'
-    label = 'examples_bpm'
