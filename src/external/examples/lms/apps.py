9422613a64a6f827570cc49a627b09527f2ddda5
ZeNchPR <tabola2005@mail.ru>
2025-06-18 00:44:55 +0300

    Добавлена api часть модуля lms

--- src/external/examples/lms/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class ExamplesLmsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.external.examples.lms'
-    label = 'examples_lms'
