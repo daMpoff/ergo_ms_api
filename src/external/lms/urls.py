cd8ab066965df6e926432165d3f017a8f75c05d2
DohaoSTR <muzalevskij.evgenij@mail.ru>
2025-06-19 16:28:21 +0300

    refactor: удаление лишних привязок к api.

--- src/external/lms/urls.py
+++ src/external/lms/urls.py
@@ -7,10 +7,4 @@ urlpatterns = [
     # Analytics endpoints
     path('analytics/student_stats/', AnalyticsViewSet.as_view({'get': 'student_stats'})),
     path('analytics/teacher_stats/', AnalyticsViewSet.as_view({'get': 'teacher_stats'})),
-
-    # Include submodules
-    path('assignments/', include('src.external.lms.assignments.urls')),
-    path('assessment/', include('src.external.lms.assessment.urls')),
-    path('courses/', include('src.external.lms.courses.urls')),
-    path('users/', include('src.external.lms.users.urls')),
 ]
