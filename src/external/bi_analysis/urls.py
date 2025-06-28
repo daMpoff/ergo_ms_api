318afacbabaa748b9a57d87fce275f882549dcfe
DohaoSTR <muzalevskij.evgenij@mail.ru>
2025-06-28 12:34:23 +0300

    modified:   src/external/bi_analysis/urls.py

--- src/external/bi_analysis/urls.py
+++ src/external/bi_analysis/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    
+    path('bi_connections/', include('src.external.bi_analysis.bi_connections.urls')),
+    path('bi_datasets/', include('src.external.bi_analysis.bi_datasets.urls')),
+    path('bi_charts/', include('src.external.bi_analysis.bi_charts.urls')),
 ]
