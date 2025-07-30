7d16121be51ea91172392f1f61b9578aa3ddf446
DohaoSTR <muzalevskij.evgenij@mail.ru>
2025-07-31 00:33:18 +0300

    build: автоматизация импортов настроек из config/settings и оптимизация работы jyputer

--- src/config/settings/jupyter.py
+++ src/config/settings/jupyter.py
@@ -1,18 +1,56 @@
+from src.config.settings.base import BASE_DIR
+from src.config.env import env
+
 try:
     import jupyterlab
     notebook_default_url = '/lab'  # Using JupyterLab
 except ImportError:
     notebook_default_url = '/tree'  # Using Jupyter
 
-PATH_TO_NOTEBOOK_DIR = 'C:/projects/ergo_ms/api/src'
+# Путь к корневой директории проекта в Jupyter.
+PATH_TO_NOTEBOOK_DIR = str(BASE_DIR)
 
+# Хост сервера jupyter, полученный из переменной окружения.
+JUPYTER_HOST = env.str('JUPYTER_HOST', default='localhost')
+
+# Порт сервера jupyter, полученный из переменной окружения.
+JUPYTER_PORT = env.str('JUPYTER_PORT', default='8002')
+
+# Аргументы для запуска сервера jupyter.
 NOTEBOOK_ARGUMENTS = [
-    '--ip', '0.0.0.0',
-    '--port', '8888',
+    '--ip', JUPYTER_HOST,
+    '--port', JUPYTER_PORT,
     '--notebook-dir', PATH_TO_NOTEBOOK_DIR,
     '--NotebookApp.default_url', notebook_default_url,
+    '--NotebookApp.allow_origin', '*',
+    '--NotebookApp.allow_remote_access', 'True',
+    '--NotebookApp.open_browser', 'False',
+    '--NotebookApp.token', '',
+    '--NotebookApp.password', '',
 ]
+
+# Имя ядра IPython, полученное из переменной окружения.
 IPYTHON_KERNEL_DISPLAY_NAME = 'Django Kernel'
 
-# if you want to use Chrome by default
-# os.environ.setdefault('BROWSER', 'google-chrome')
+# Настройки для интеграции с Django
+SHELL_PLUS_PRE_IMPORTS = [
+    ('django.db', ('connection', 'connections', 'reset_queries', 'close_old_connections')),
+    ('django.conf', ('settings',)),
+    ('django.core.management', ('execute_from_command_line',)),
+    ('django.core.management.base', ('BaseCommand', 'CommandError', 'CommandParser')),
+    ('django.db.models', ('Avg', 'Count', 'F', 'Max', 'Min', 'Q', 'Sum', 'Value')),
+    ('django.utils', ('timezone',)),
+    ('datetime', ('datetime', 'timedelta', 'date')),
+    ('decimal', ('Decimal',)),
+    ('functools', ('reduce',)),
+    ('itertools', ('chain', 'islice')),
+    ('json', ('dumps', 'loads')),
+    ('operator', ('itemgetter', 'attrgetter')),
+    ('pathlib', ('Path',)),
+    ('pprint', ('pprint',)),
+    ('random', ('choice', 'randint', 'random', 'sample', 'shuffle')),
+    ('string', ('ascii_letters', 'ascii_lowercase', 'ascii_uppercase', 'digits', 'hexdigits', 'octdigits', 'printable', 'punctuation', 'whitespace')),
+    ('textwrap', ('dedent', 'indent', 'shorten')),
+    ('time', ('sleep',)),
+    ('urllib.parse', ('quote_plus', 'urlencode', 'urljoin', 'urlparse', 'urlsplit', 'urlunparse')),
+]
--- src/config/settings/static.py
+++ src/config/settings/static.py
@@ -5,7 +5,7 @@
 
 import os
 
-from src.config.settings.base import BASE_DIR
+from src.config.settings.base import API_DIR
 
 # URL для доступа к статическим файлам.
 STATIC_URL = '/static/'
@@ -23,10 +23,16 @@ MEDIA_ROOT = 'media'
 LOGS_URL = '/logs/'
 
 # Корневая директория для логов.
-LOGS_ROOT = os.path.join(BASE_DIR.parent, 'logs')
+LOGS_ROOT = os.path.join(API_DIR, 'logs')
 
 # Хранилище для статических файлов, использующее Whitenoise для сжатия и кэширования.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Корневая директория для ресурсов.
-RESOURCES_DIR = os.path.join(BASE_DIR.parent, 'resources')
+RESOURCES_DIR = os.path.join(API_DIR, 'resources')
+
+# Корневая директория для обученных моделей.
+TRAINED_MODELS_PATH = os.path.join(API_DIR, 'trained_models')
+
+# Корневая директория для сторонних программ.
+PACKAGES_PATH = os.path.join(API_DIR, 'packages')
